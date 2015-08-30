bb4f5f59b0cd5d8675a0eeb3d649400787ab9989
Kuzmin Yaroslav <kuzmin.yaroslav@gmail.com>
2015-08-30 22:22:18 +0500

    добавил окно создание работы

--- job.c
+++ job.c
@@ -82,7 +82,6 @@ int callback_table(void * ud, int argc, char **argv, char ** col_name)
 /*************************************/
 char QUERY_CREATE_TABLE[]="CREATE TABLE job(name PRIMARY KEY,pressure INTEGER,time INTEGER,uprise INTEGER,lowering INTEGER)";
 
-
 /*************************************/
 int init_db(void)
 {
@@ -128,6 +127,12 @@ int deinit_db(void)
 GtkWidget * load_job_window = NULL;
 GtkWidget * create_job_window = NULL;
 
+GtkEntryBuffer * eb_name_job = NULL;
+GtkEntryBuffer * eb_pressure = NULL;
+GtkEntryBuffer * eb_time = NULL;
+GtkEntryBuffer * eb_uprise = NULL;
+GtkEntryBuffer * eb_lowering = NULL;
+
 /*****************************************************************************/
 void load_job(GtkButton * b,gpointer d)
 {
@@ -140,15 +145,15 @@ void load_job_window_destroy(GtkWidget * w,gpointer d)
 	g_message("load_job_window destroy");
 }
 
-
 void create_job(GtkButton *b,gpointer d)
 {
+	gtk_widget_destroy(create_job_window);
 	g_message("Сохранить работу");
 }
 
 void create_job_window_destroy(GtkWidget * w,gpointer d)
 {
-	g_message("load_job_window destroy");
+	g_message("create_job_window destroy");
 }
 /*****************************************************************************/
 char STR_LOAD_JOB_WINDOW[] = "Загрузить работу";
@@ -179,17 +184,91 @@ void create_window_load_job(GtkMenuItem * b,gpointer d)
 
 void create_window_create_job(GtkMenuItem * b,gpointer d)
 {
-	/*
-	GtkWidget * hbox;
-	GtkWidget * save_job;
-*/
+	GtkWidget * cwgrid;
+	GtkWidget * b_save_job;
+
+	GtkWidget * l_name_job;
+	GtkWidget * l_pressure;
+	GtkWidget * l_time;
+	GtkWidget * l_uprise;
+	GtkWidget * l_lowering;
+	GtkWidget * e_name_job;
+	GtkWidget * e_pressure;
+	GtkWidget * e_time;
+	GtkWidget * e_uprise;
+	GtkWidget * e_lowering;
+
 	create_job_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_screen(GTK_WINDOW (create_job_window),gtk_widget_get_screen(main_window));
 	gtk_window_set_title(GTK_WINDOW(create_job_window),STR_CREATE_JOB_WINDOW);
 	gtk_container_set_border_width(GTK_CONTAINER(create_job_window),5);
-
 	g_signal_connect (create_job_window,"destroy",G_CALLBACK(create_job_window_destroy),NULL);
 
+	cwgrid = gtk_grid_new();
+	gtk_container_add(GTK_CONTAINER(create_job_window),cwgrid);
+	gtk_container_set_border_width(GTK_CONTAINER(cwgrid),5);
+
+	b_save_job = gtk_button_new_with_label("сохранить");
+	gtk_grid_attach(GTK_GRID(cwgrid),b_save_job,2,2,1,1);
+	g_signal_connect(b_save_job,"clicked",G_CALLBACK(create_job),NULL);
+
+	l_name_job = gtk_label_new("Наименование работы");
+	eb_name_job = gtk_entry_buffer_new("------",-1);
+	e_name_job = gtk_entry_new_with_buffer(eb_name_job);
+	gtk_entry_set_width_chars(GTK_ENTRY(e_name_job),30);
+	gtk_widget_set_halign(e_name_job,GTK_ALIGN_CENTER);
+	gtk_grid_attach(GTK_GRID(cwgrid),l_name_job,0,0,2,1);
+	gtk_grid_attach(GTK_GRID(cwgrid),e_name_job,0,1,2,1);
+
+	l_pressure = gtk_label_new("Давление");
+	eb_pressure = gtk_entry_buffer_new("2",-1);
+	gtk_entry_buffer_set_max_length(GTK_ENTRY_BUFFER(eb_pressure),1);
+	e_pressure = gtk_entry_new_with_buffer(eb_pressure);
+	gtk_entry_set_width_chars(GTK_ENTRY(e_pressure),3);
+	gtk_widget_set_halign(e_pressure,GTK_ALIGN_CENTER);
+	gtk_grid_attach(GTK_GRID(cwgrid),l_pressure,2,0,1,1);
+	gtk_grid_attach(GTK_GRID(cwgrid),e_pressure,2,1,1,1);
+
+	l_time = gtk_label_new("Время работы");
+	eb_time = gtk_entry_buffer_new("00:00:00",-1);
+	gtk_entry_buffer_set_max_length(GTK_ENTRY_BUFFER(eb_time),8);
+	e_time = gtk_entry_new_with_buffer(eb_time);
+	gtk_entry_set_width_chars(GTK_ENTRY(e_time),10);
+	gtk_widget_set_halign(e_time,GTK_ALIGN_CENTER);
+	gtk_grid_attach(GTK_GRID(cwgrid),l_time,3,0,1,1);
+	gtk_grid_attach(GTK_GRID(cwgrid),e_time,3,1,1,1);
+
+	l_uprise = gtk_label_new("Угол подъема");
+	eb_uprise = gtk_entry_buffer_new("000",-1);
+	gtk_entry_buffer_set_max_length(GTK_ENTRY_BUFFER(eb_uprise),3);
+	e_uprise = gtk_entry_new_with_buffer(eb_uprise);
+	gtk_entry_set_width_chars(GTK_ENTRY(e_uprise),5);
+	gtk_widget_set_halign(e_uprise,GTK_ALIGN_CENTER);
+	gtk_grid_attach(GTK_GRID(cwgrid),l_uprise,4,0,1,1);
+	gtk_grid_attach(GTK_GRID(cwgrid),e_uprise,4,1,1,1);
+
+	l_lowering = gtk_label_new("Угол опускания");
+	eb_lowering = gtk_entry_buffer_new("000",-1);
+	gtk_entry_buffer_set_max_length(GTK_ENTRY_BUFFER(eb_lowering),3);
+	e_lowering = gtk_entry_new_with_buffer(eb_lowering);
+	gtk_entry_set_width_chars(GTK_ENTRY(e_lowering),5);
+	gtk_widget_set_halign(e_lowering,GTK_ALIGN_CENTER);
+	gtk_grid_attach(GTK_GRID(cwgrid),l_lowering,5,0,1,1);
+	gtk_grid_attach(GTK_GRID(cwgrid),e_lowering,5,1,1,1);
+
+	gtk_widget_show(l_lowering);
+	gtk_widget_show(l_uprise);
+	gtk_widget_show(l_time);
+	gtk_widget_show(l_pressure);
+	gtk_widget_show(l_name_job);
+	gtk_widget_show(e_lowering);
+	gtk_widget_show(e_uprise);
+	gtk_widget_show(e_time);
+	gtk_widget_show(e_pressure);
+	gtk_widget_show(e_name_job);
+	gtk_widget_show(b_save_job);
+	gtk_widget_show(cwgrid);
+	gtk_widget_show(create_job_window);
 	g_message("Cоздать работу");
 }
 
